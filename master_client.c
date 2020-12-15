7fc81a822c57a7adbdbfb38259fdc0c0f14016c1
clementine <clementineguillot@gmail.com>
2020-12-15 12:23:20 +0100

    fonction manip tube

--- master_client.c
+++ master_client.c
@@ -16,20 +16,34 @@
 
 
 //==========================================================================
-//création et ouverture tubes nommés
+//création tubes nommés
 
-int create_tube1()
+void create_tube1()
 {
-  mkfifo(TUBE_CLIENT_MASTER, 0666);
+  int tube1 = mkfifo(TUBE_CLIENT_MASTER, 0666);
+  assert(tube1 != -1);
+  printf("le tube client master vient d'etre crée\n");
+}
+
+void create_tube2(){
+  int tube2 = mkfifo(TUBE_MASTER_CLIENT, 0666);
+  assert(tube2 != -1);
+  printf("le tube master client vient d'etre crée");
+}
+
+//==========================================================================
+//ouverture tubes nommés
+
+int open_tube1()
+{
   int tube1 = open(TUBE_CLIENT_MASTER, O_RDONLY);
   assert(tube1 != -1);
   printf("le tube vient d'etre ouvert en lecture\n");
-
   return tube1;
 }
 
-int create_tube2(){
-  mkfifo(TUBE_MASTER_CLIENT, 0666);
+int open_tube2()
+{
   int tube2 = open(TUBE_MASTER_CLIENT, O_WRONLY);
   assert(tube1 != -1);
   printf("le tube vient d'etre ouvert en ecriture\n");
@@ -39,8 +53,20 @@ int create_tube2(){
 //========================================================================
 //fermeture tube nommés
 
-void closetube(int tube){
+void closetube(int tube)
+{
   int g = close(tube);
   assert(g == 0);
   printf("le tube est fermer");
 }
+
+//========================================================================
+//écriture dans un tube
+
+void write_tube(int tube, int order)
+{
+  write(tube,&order,1);
+}
+
+//========================================================================
+//lecture dans un tube
